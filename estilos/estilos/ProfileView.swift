7a76328e92c41aeae957fc86f4d20f74ebef5008
MikeDevUcamp <115725749+MikeDevUcamp@users.noreply.github.com>
2023-04-24 11:18:23 -0600

    fondo

--- estilos/estilos/ProfileView.swift
+++ estilos/estilos/ProfileView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("profile_background")
+                .resizable()
+                .scaledToFill()
+                .blur(radius: 10)
+                .ignoresSafeArea()
+                
+        }
+        VStack(spacing: 20){
+            Image("profile_pic")
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle().stroke( Color.white,lineWidth: 5))
+                .shadow(radius: 10)
+                .padding()
+            Text("Miguel Gómez")
+                .font(.title)
+                .foregroundColor(.purple   )
+            Text("miguel@gmail.com")
+                .font(.subheadline)
+                .foregroundColor(.purple)
+            Button(action: {
+                
+            }, label: {
+                Text("Log out")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.purple)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+            })
+        }
     }
 }
 
